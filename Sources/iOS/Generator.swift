58586998d7a775b1986bb110f2effc8166741805
Khoa Pham <onmyway133@gmail.com>
2019-03-19 14:21:48 +0100

    Add many draw functions to Generator

--- Sources/iOS/Generator.swift
+++ Sources/iOS/Generator.swift
@@ -8,13 +8,53 @@
 
 import JavaScriptCore
 
+public struct Point {
+  let x: Float
+  let y: Float
+  
+  func toRoughPoint() -> [Float] {
+    return [x, y]
+  }
+}
+
 public class Generator {
-  let generator: JSValue
-  init(generator: JSValue) {
+  private let generator: JSValue
+  public init(generator: JSValue) {
     self.generator = generator
   }
   
-  func rectangle(frame: CGRect) {
-    
+  public func rectangle(x: Float, y: Float, width: Float, height: Float) {
+    let drawble = generator.invokeMethod("rectangle", withArguments: [x, y, width, height])
+  }
+  
+  public func ellipse(x: Float, y: Float, width: Float, height: Float) {
+    let drawble = generator.invokeMethod("ellipse", withArguments: [x, y, width, height])
+  }
+  
+  public func circle(x: Float, y: Float, diameter: Float) {
+    let drawable = generator.invokeMethod("circle", withArguments: [x, y, diameter])
+  }
+  
+  public func linearPath(points: [Point]) {
+    let roughPoints = points.map({ $0.toRoughPoint() })
+    let drawble = generator.invokeMethod("linearPath", withArguments: roughPoints)
+  }
+  
+  public func arc(x: Float, y: Float, width: Float, height: Float, start: Float, stop: Float, closed: Bool = false) {
+    let drawble = generator.invokeMethod("arc", withArguments: [x, y, width, height, start, stop, closed])
+  }
+  
+  public func curve(points: [Point]) {
+    let roughPoints = points.map({ $0.toRoughPoint() })
+    let drawble = generator.invokeMethod("curve", withArguments: [roughPoints])
+  }
+  
+  public func polygon(points: [Point]) {
+    let roughPoints = points.map({ $0.toRoughPoint() })
+    let drawble = generator.invokeMethod("polygon", withArguments: roughPoints)
+  }
+  
+  public func path(d: String) {
+    let drawble = generator.invokeMethod("path", withArguments: [d])
   }
 }
