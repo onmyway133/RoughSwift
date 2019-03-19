545061177a68c5490647e470fe2ef49fcc748e0c
Khoa Pham <onmyway133@gmail.com>
2019-03-19 14:35:26 +0100

    Convert to rough

--- Sources/iOS/Options.swift
+++ Sources/iOS/Options.swift
@@ -8,6 +8,17 @@
 
 import Foundation
 
+public enum FillStyle: String {
+  case hachure
+  case solid
+  case zigzag
+  case crossHatch = "cross-hatch"
+  case dots
+  case sunburst
+  case dashed
+  case zigzagLine = "zigzag-line"
+}
+
 public struct Options {
   public var maxRandomnessOffset: Float = 2
   public var roughness: Float = 1
@@ -16,11 +27,30 @@ public struct Options {
   public var strokeWidth: Float = 1
   public var curveTightness: Float = 0
   public var curveStepCount: Float = 9
-  public var fillStyle: String = "hachure"
+  public var fillStyle: FillStyle = .hachure
   public var fillWeight: Float = -1
   public var hachureAngle: Float = -41
   public var hachureGap: Float = -1
   public var dashOffset: Float = -1
   public var dashGap: Float = -1
   public var zigzagOffset: Float = -1
+  
+  func toRoughDictionary() -> [String: Any] {
+    return [
+      "maxRandomnessOffset": maxRandomnessOffset,
+      "roughness": roughness,
+      "bowing": bowing,
+      "stroke": stroke,
+      "strokeWidth": strokeWidth,
+      "curveTightness": curveTightness,
+      "curveStepCount": curveStepCount,
+      "fillStyle": fillStyle.rawValue,
+      "fillWeight": fillWeight,
+      "hachureAngle": hachureAngle,
+      "hachureGap": hachureGap,
+      "dashOffset": dashOffset,
+      "dashGap": dashGap,
+      "zigzagOffset": zigzagOffset
+    ]
+  }
 }
