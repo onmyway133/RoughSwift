7c16cca715338c12e56859d32414411de7fcfb93
Khoa Pham <onmyway133@gmail.com>
2019-03-20 15:25:39 +0100

    Play around in Playground

--- Playground-iOS.playground/Contents.swift
+++ Playground-iOS.playground/Contents.swift
@@ -11,7 +11,17 @@ view.backgroundColor = .white
 let layer = draw(size: size, using: { generator in
   var options = Options()
   options.fill = UIColor.red
+  options.fillStyle = .zigzag
   generator.circle(x: 200, y: 200, diameter: 80, options: options)
+    
+  options.fillStyle = .sunburst
+  options.hachureAngle = 120
+  options.hachureGap = 4
+  generator.rectangle(x: 10, y: 10, width: 100, height: 50, options: options)
+    
+  options.fill = UIColor.green
+  options.fillStyle = .crossHatch
+  generator.ellipse(x: 80, y: 100, width: 100, height: 50, options: options)
 })
 
 view.layer.addSublayer(layer)
