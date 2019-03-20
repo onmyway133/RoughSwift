c393aa5fea32c59e7d5e11d7ac01099a1ebed55d
Khoa Pham <onmyway133@gmail.com>
2019-03-20 10:48:08 +0100

    Try iOS Playground

--- Playground-iOS.playground/Contents.swift
+++ Playground-iOS.playground/Contents.swift
@@ -2,5 +2,15 @@
 
 import UIKit
 import RoughSwift
+import PlaygroundSupport
 
-var str = "Hello, playground"
+let size = CGSize(width: 300, height: 300)
+let view = UIView(frame: CGRect(origin: .zero, size: size))
+let engine = Engine()
+let generator = engine.generator(size: size)
+let renderer = Renderer(layer: view.layer)
+generator.onDrawable = renderer.handle
+
+generator.rectangle(x: 10, y: 10, width: 50, height: 50)
+
+PlaygroundPage.current.liveView = view
