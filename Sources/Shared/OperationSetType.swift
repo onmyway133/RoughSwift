5369ebd7c741b2cffd269c90a30d9162dd64d218
Khoa Pham <onmyway133@gmail.com>
2019-03-21 12:46:41 +0100

    Add documentation

--- Sources/Shared/OperationSetType.swift
+++ Sources/Shared/OperationSetType.swift
@@ -9,9 +9,14 @@
 import Foundation
 
 public enum OperationSetType: String {
+  /// border
   case path
+  /// solid fill
   case fillPath
+  /// sketch fill
   case fillSketch
+  /// svg path solid fill
   case path2DFill = "path2Dfill"
+  /// svg path sketch fill
   case path2DPattern = "path2Dpattern"
 }
