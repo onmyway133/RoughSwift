cc5fc10eb4ed8e90e3520129ab90751eaa36135a
Khoa Pham <onmyway133@gmail.com>
2019-03-21 13:53:15 +0100

    Update playground

--- Playground-iOS.playground/Contents.swift
+++ Playground-iOS.playground/Contents.swift
@@ -28,15 +28,14 @@ let layer = draw(size: size, using: { generator in
   generator.ellipse(x: 80, y: 170, width: 100, height: 50, options: options)
 })
 
-let apple = draw(size: CGSize(width: 500, height: 500), using: { generator in
+let apple = draw(size: CGSize(width: 150, height: 150), using: { generator in
   var options = Options()
-  options.stroke = UIColor.red
-  options.fill = UIColor.purple
-  let apple = "M809.333 349q-21-31-49-52.5t-55.5-31-46.5-13.5-36-4q-35-4-75 7.5t-73 25-48 13.5q-17 0-75-22.5t-94-21.5q-65 1-121.5 35.5t-90.5 92.5q-32 57-40 129t5.5 141.5 39.5 133 60 112.5q20 30 34.5 48t36 40 44.5 32.5 46 9.5q30-1 78.5-21.5t89.5-20.5q40 0 88 20.5t81 20.5q25-1 47-11t42.5-30.5 33.5-37.5 34-47q43-64 71-146-8-3-17.5-8t-33-22.5-40.5-38.5-31.5-58-15.5-80q0-36 11.5-69.5t25-53.5 34-39 26.5-23.5 14-9.5zm-186-349q-39 2-81 23.5t-70 54.5q-27 30-41.5 73t-9.5 85q41 3 80.5-17.5t68.5-54.5q28-35 43-78t10-86z"
+  options.stroke = UIColor.brown
+  options.fill = UIColor.red
+  let apple = "M85 32C115 68 239 170 281 192 311 126 274 43 244 0c97 58 146 167 121 254 28 28 40 89 29 108 -25-45-67-39-93-24C176 409 24 296 0 233c68 56 170 65 226 27C165 217 56 89 36 54c42 38 116 96 161 122C159 137 108 72 85 32z"
   generator.path(d: apple, options: options)
 })
 
-apple.setAffineTransform(CGAffineTransform(scaleX: 0.15, y: 0.15))
 view.layer.addSublayer(layer)
 view.layer.addSublayer(apple)
 PlaygroundPage.current.liveView = view
